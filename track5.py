ded6cb5cdc84501bd552147ecc1b1573910519b7
jieunlee <jieun3181@gmail.com>
2022-07-17 21:34:25 +0900

    swea 1946-3

--- track5.py
+++ track5.py
@@ -1,20 +1,4 @@
-
-
-
-T = int(input())
- 
-for tc in range(T):
-    data = ''
-    N = int(input())
-    for _ in range(N):
-        chr, num = map(str, input().split())
-        data += chr * int(num)
- 
-    print('#{}'.format(tc+1))
-    for i in range(0, len(data), 10):
-        print(data[i:i+10])
-
-
+#1946
 
 
 T = int(input())
